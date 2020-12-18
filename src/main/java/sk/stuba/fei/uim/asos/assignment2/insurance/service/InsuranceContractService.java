c52d9a89282f2b84bfb03536539ab0b5996251cd
Milan Mladoniczky <6153201+mladoniczky@users.noreply.github.com>
2020-12-18 20:21:08 +0100

    - fix insurance services

--- src/main/java/sk/stuba/fei/uim/asos/assignment2/insurance/service/InsuranceContractService.java
+++ src/main/java/sk/stuba/fei/uim/asos/assignment2/insurance/service/InsuranceContractService.java
@@ -1,33 +1,42 @@
 package sk.stuba.fei.uim.asos.assignment2.insurance.service;
 
-public class InsuranceContractService {
-//
-//    private final AtomicLong idCounter;
-//    private final Map<Long, InsuranceBase> contracts;
-//
-//    public InsuranceContractService() {
-//        idCounter = new AtomicLong(0);
-//        contracts = new HashMap<>();
-//    }
-//
-//    public InsuranceBase create(InsuranceBase contract) {
-//        contract.setId(idCounter.incrementAndGet());
-//        contracts.put(contract.getId(), contract);
-//        return contract;
-//    }
-//
-//    public InsuranceBase update(InsuranceBase contract) {
-//        if (!contracts.containsKey(contract.getId()))
-//            throw new IllegalArgumentException("Contract id:" + contract.getId() + " has not been found!");
-//        return contracts.put(contract.getId(), contract);
-//    }
-//
-//    public List<InsuranceBase> getAll() {
-//        return new ArrayList<>(contracts.values());
-//    }
-//
-//    public List<InsuranceBase> getByUserId(Long userId) {
-//        return contracts.values().stream().filter(insuranceBase -> insuranceBase.getInsurerId() == userId)
-//                .collect(Collectors.toList());
-//    }
+import sk.stuba.fei.uim.asos.assignment2.ws.AbstractInsurance;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+public class InsuranceContractService implements IInsuranceContractService<AbstractInsurance>{
+
+    private final AtomicLong idCounter;
+    private final Map<Long, AbstractInsurance> contracts;
+
+    public InsuranceContractService() {
+        idCounter = new AtomicLong(0);
+        contracts = new HashMap<>();
+    }
+
+    public AbstractInsurance create(AbstractInsurance contract) {
+        contract.setId(idCounter.incrementAndGet());
+        contracts.put(contract.getId(), contract);
+        return contract;
+    }
+
+    public AbstractInsurance update(AbstractInsurance contract) {
+        if (!contracts.containsKey(contract.getId()))
+            throw new IllegalArgumentException("Contract id:" + contract.getId() + " has not been found!");
+        return contracts.put(contract.getId(), contract);
+    }
+
+    public List<AbstractInsurance> getAll() {
+        return new ArrayList<>(contracts.values());
+    }
+
+    public List<AbstractInsurance> getByUserId(Long userId) {
+        return contracts.values().stream().filter(insuranceBase -> insuranceBase.getInsurerId() == userId)
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/sk/stuba/fei/uim/asos/assignment2/insurance/web/InsuranceServicePort.java
+++ src/main/java/sk/stuba/fei/uim/asos/assignment2/insurance/web/InsuranceServicePort.java
@@ -3,44 +3,89 @@ package sk.stuba.fei.uim.asos.assignment2.insurance.web;
 import sk.stuba.fei.uim.asos.assignment2.insurance.service.InsuranceContractService;
 import sk.stuba.fei.uim.asos.assignment2.ws.*;
 
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.xml.bind.annotation.XmlSeeAlso;
-import javax.xml.ws.Holder;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@WebService(name = "InsuranceServicePortType", targetNamespace = "insurances", serviceName = "InsuranceService")
+@WebService(name = "InsuranceServicePortType", targetNamespace = "insurances-service", serviceName = "InsuranceService")
 @SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE)
 @XmlSeeAlso({ObjectFactory.class})
-public class InsuranceServicePort {
+public class InsuranceServicePort implements InsuranceServicePortType {
 
-//    private final InsuranceContractService insuranceService;
-//
-//    public InsuranceServicePort(InsuranceContractService insuranceService) {
-//        this.insuranceService = insuranceService;
-//    }
-//
-//    @Override
-//    public Insurances getAll(long id) {
-//        Insurances insurances = new Insurances();
-//        List<InsuranceBase> insuranceList = insuranceService.getByUserId(id);
-//        if (insuranceList.isEmpty())
-//            return insurances;
-//
-//
-//        return insurances;
-//    }
-//
-//    @Override
-//    public void add(Holder<InsuranceType> insurance) {
-//        if (insurance.value.getInjuryInsurance() != null)
-//            insuranceService.create(insurance.value.getInjuryInsurance());
-//        if (insurance.value.getTravelInsurance() != null)
-//            insuranceService.create(insurance.value.getTravelInsurance());
-//        if (insurance.value.getHouseholdInsurance() != null)
-//            insuranceService.create(insurance.value.getHouseholdInsurance());
-//        if (insurance.value.getHouseInsurance() != null)
-//            insuranceService.create(insurance.value.getHouseInsurance());
-//    }
+    private final InsuranceContractService insuranceService;
+
+    public InsuranceServicePort() {
+        this.insuranceService = new InsuranceContractService();
+    }
+
+    public InsuranceServicePort(InsuranceContractService insuranceService) {
+        this.insuranceService = insuranceService;
+    }
+
+    @Override
+    @WebMethod
+    @WebResult(name = "insurance", targetNamespace = "insurances", partName = "body")
+    public InsuranceType add(InsuranceType parameter) {
+        return toInsuranceType(insuranceService.create(toInsurance(parameter)));
+    }
+
+    @Override
+    @WebMethod
+    @WebResult(name = "insurance", targetNamespace = "insurances", partName = "body")
+    public InsuranceType update(InsuranceType parameter) {
+        return toInsuranceType(insuranceService.update(toInsurance(parameter)));
+    }
+
+    @Override
+    @WebMethod
+    @WebResult(name = "insurances", targetNamespace = "insurances", partName = "body")
+    public Insurances all() {
+        return toInsurances(insuranceService.getAll());
+    }
+
+    @Override
+    @WebMethod
+    @WebResult(name = "insurances", targetNamespace = "insurances", partName = "body")
+    public Insurances byUser(long parameter) {
+        return toInsurances(insuranceService.getByUserId(parameter));
+    }
+
+    private AbstractInsurance toInsurance(InsuranceType type) {
+        if (type.getTravelInsurance() != null)
+            return type.getTravelInsurance();
+        if (type.getInjuryInsurance() != null)
+            return type.getInjuryInsurance();
+        if (type.getHouseholdInsurance() != null)
+            return type.getHouseholdInsurance();
+        if (type.getHouseInsurance() != null)
+            return type.getHouseInsurance();
+        throw new UnsupportedOperationException("Application cannot resolve request insurance type");
+    }
+
+    private InsuranceType toInsuranceType(AbstractInsurance insurance) {
+        InsuranceType insuranceType = new InsuranceType();
+        if (insurance instanceof TravelInsurance) {
+            insuranceType.setTravelInsurance((TravelInsurance) insurance);
+        }
+        if (insurance instanceof InjuryInsurance) {
+            insuranceType.setInjuryInsurance((InjuryInsurance) insurance);
+        }
+        if (insurance instanceof HouseholdInsurance) {
+            insuranceType.setHouseholdInsurance((HouseholdInsurance) insurance);
+        }
+        if (insurance instanceof HouseInsurance) {
+            insuranceType.setHouseInsurance((HouseInsurance) insurance);
+        }
+        return insuranceType;
+    }
+
+    private Insurances toInsurances(List<AbstractInsurance> insurances) {
+        Insurances ins = new Insurances();
+        ins.getInsurance().addAll(insurances.stream().map(this::toInsuranceType).collect(Collectors.toList()));
+        return ins;
+    }
 }
